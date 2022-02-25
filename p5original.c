4bb89e383eb5644e07ee4b0c45d63b7d2e032b80
Lokesh J <97089488+Lokeh123@users.noreply.github.com>
2022-02-25 04:55:30 +0000

    woihfih

---
+++ p5original.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int input_array_size()
+{
+  intn;
+  printf("enter the array size:\n"):
+  scanf("%d",&n);
+  return n;
+  }
+void int_array(int n,int a[n]);
+{
+for(int i=0;i<n;i++)
+  a[i]
+  }
